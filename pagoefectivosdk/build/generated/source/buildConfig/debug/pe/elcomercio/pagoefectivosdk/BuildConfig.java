e44b7ce415d15cf99b6cded79a5b1c5410654335
Carlos Leonardo Camilo Vargas Huaman <carlosleonardovargashuaman@gmail.com>
2020-04-03 23:15:31 -0500

    Add Android application in order to improve it continuously

---
+++ pagoefectivosdk/build/generated/source/buildConfig/debug/pe/elcomercio/pagoefectivosdk/BuildConfig.java
@@ -0,0 +1,25 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package pe.elcomercio.pagoefectivosdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "pe.elcomercio.pagoefectivosdk";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "pe.elcomercio.pagoefectivosdk";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 110000;
+  public static final String VERSION_NAME = "1.0.10";
+  // Fields from build type: debug
+  public static final String AUTHORIZATIONS = "authorizations";
+  public static final String CIPS = "cips";
+  public static final String DOMAIN_PRE = "pre1a.services.pagoefectivo.pe";
+  public static final String DOMAIN_PRO = "services.pagoefectivo.pe";
+  public static final String URL_BASE_PRE = "https://pre1a.services.pagoefectivo.pe/v1/";
+  public static final String URL_BASE_PRO = "https://services.pagoefectivo.pe/v1/";
+}
